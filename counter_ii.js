dba587638b36a31bda540ee2c5965320ac8da0cb
gade shiva kumar <119879719+gadeshivakumar@users.noreply.github.com>
2024-01-11 22:23:49 +0530

    Add files via upload

---
+++ counter_ii.js
@@ -0,0 +1,31 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let c=init;    
+    return{
+        increment : function(){
+            c++;
+            return c;
+        },
+        
+        decrement: function()
+        {
+            c--;
+            return c;
+        },
+
+        reset: function()
+        {c=init;
+            return c;
+        }
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
